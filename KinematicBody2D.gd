a027b0b4f253a554de7565b57a75cf37df398ee3
Ruion <liewyeat@gmail.com>
2019-10-19 11:17:08 +0800

    Added platforms, variables, jump and fall to player

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -1,18 +1,27 @@
 extends KinematicBody2D
 
+export var speed = 150
+export var jumpheight = -350
+export var gravity = 10
+
+const UP = Vector2(0, -1);
 var motion = Vector2();
 
 func _physics_process(delta):
 	
-	motion.y +=10
+	motion.y += gravity
 	
 	if(Input.is_action_pressed("ui_right")):
-		motion.x = 100;
+		motion.x = speed
 	elif(Input.is_action_pressed("ui_left")):
-		motion.x = -100;	
+		motion.x = -speed
 	else:
-		motion.x = 0;
+		motion.x = 0
+	
+	if(is_on_floor()):
+		if(Input.is_action_just_pressed("ui_up")):
+			motion.y = jumpheight
 			
-	move_and_slide(motion);
+	motion = move_and_slide(motion, UP);
 		
 	pass
